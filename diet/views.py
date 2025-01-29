7c9ce2b508f7242e6a2f33ad3ecdb8ff13151a84
kenken20081208 <pingyej363@gmail.com>
2025-01-29 10:48:15 +0900

    大体形はできた

--- diet/views.py
+++ diet/views.py
@@ -1,12 +1,16 @@
 from django.http import HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.urls import reverse
 from .models import Meal
 
 
 def index(request):
     all_meal = Meal.objects.all()
-    context = {"all_meal": all_meal}
+    total = 0
+    for meal in all_meal:
+        total = total + meal.kcal
+
+    context = {"all_meal": all_meal, "total": total}
     return render(request, "diet/input.html", context)
 
 
@@ -15,7 +19,7 @@ def create(request):
     kcal = request.POST.get("kcal")
     new_meal = Meal(name=name, kcal=kcal)
     new_meal.save()
-    return HttpResponseRedirect(reverse("diet:indx"))
+    return HttpResponseRedirect(reverse("diet:index"))
 
 
 def sum(request):
